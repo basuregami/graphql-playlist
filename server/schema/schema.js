60be18d17dbfca4e63c067ebfe886978e5d24d1b
basuregamiolive <basu.regmi@olivemedia.co>
2019-02-26 15:45:50 +0545

    chapter 12 introduction t nested data in graphql

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -12,9 +12,9 @@ const {
 
 //faker data
 var books = [
-    { name: 'Name of the wind', genre: 'Fantasy', id: '1'},
-    { name: 'The final Empire', genre: 'Fantasy', id: '2'},
-    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3'}
+    { name: 'Name of the wind', genre: 'Fantasy', id: '1', authorId: '1'},
+    { name: 'The final Empire', genre: 'Fantasy', id: '2', authorId: '2'},
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3'}
 
 ]
 
@@ -29,7 +29,13 @@ const BookType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    genre: { type: GraphQLString }
+    genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args){
+        return _.find(authors, {id: parent.authorId})
+      }
+    }
   })
 });
 
