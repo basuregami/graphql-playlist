f005f4507f88ecbacc40334a6fbc6ae69129edec
basuregamiolive <basu.regmi@olivemedia.co>
2019-02-26 16:48:01 +0545

    moongose connection setup

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const schema = require('./schema/schema');
 const graphqlHTTP = require('express-graphql');
 
 const app = express();
+
+
+//connect to database
+mongoose.connect('mongodb+srv://root:root@cluster0-qpftn.mongodb.net/test?retryWrites=true')
+mongoose.connection.once('open', () => {
+	console.log('connected to database')
+});
 
 app.use('/graphql',graphqlHTTP({
 	schema,
